c3fd4d05ad636bd06716cfefec3e6770fb8a9bd1
Raphael Cabrera <raphaelcabrera@RaphaelrerasMBP.attlocal.net>
2024-03-31 19:35:56 -0500

    completed chapter 16

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+ 
+
 export default function Customers() {
   return (
     <div>Customers Page</div>
